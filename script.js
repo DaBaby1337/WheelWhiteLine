37d0c933e5ec1b6061d4e3e023968ad67b6cc5c3
Nikita Denisov <rok-08@bk.ru>
2022-03-21 18:56:30 +0400

    slider-car-done

--- script.js
+++ script.js
@@ -67,4 +67,42 @@ window.onscroll=()=>{
     }
 };
 }())
-// header-start
+// header-end
+// slider-cars-start
+const imagesCar= document.querySelectorAll('.slider-car .slider-line-car img');
+const sliderLineCar= document.querySelector('.slider-line-car');
+let countCar=0;
+let widthCar;
+function initCar(){
+    console.log('resize');
+    widthCar = document.querySelector('.slider-car').offsetWidth;
+    sliderLineCar.style.width=widthCar*imagesCar.length + 'px';
+    imagesCar.forEach(item=>{
+        item.style.width=widthCar +'px';
+        item.style.height='auto';
+    })
+    rollSliderCar();
+}
+window.addEventListener('resize',initCar);
+initCar();
+
+document.querySelector('.slider-next-car').addEventListener('click', function(){
+    countCar++;
+    if (countCar>= imagesCar.length){
+        countCar=0;
+    }
+rollSliderCar();
+})
+
+document.querySelector('.slider-back-car').addEventListener('click', function(){
+    countCar--;
+    if (countCar <0 ){
+        countCar=imagesCar.length-1;
+    }
+rollSliderCar();
+})
+
+function rollSliderCar(){
+    sliderLineCar.style.transform ='translate(-' +countCar*widthCar +'px';
+}
+//slider-cars-end
